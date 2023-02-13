03ba8abda15457ccf0dff48e4fed23d4fac7bc2e
Paras Gediya <mrparas103@gmail.com>
2023-02-13 17:45:27 +0530

    add security group to access instance

--- main.tf
+++ main.tf
@@ -56,9 +56,51 @@ data "aws_subnet" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc_data.id
 }
 
+resource "aws_security_group" "instance_security_group" {
+  name        = "instance_security_group"
+  description = "security group that allow server on port 8080"
+  vpc_id      = data.aws_vpc.default_vpc_data.id
+
+  ingress {
+    description      = "https port"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "http port"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "ssh port"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_instance" "pokemon_app_ec2" {
-  ami           = data.aws_ami.ubuntu.id
-  subnet_id     = data.aws_subnet.default_subnet.id
-  instance_type = "t2.micro"
-  key_name      = "aayush_paras"
+  ami             = data.aws_ami.ubuntu.id
+  subnet_id       = data.aws_subnet.default_subnet.id
+  instance_type   = "t2.micro"
+  key_name        = "aayush_paras"
+  security_groups = [aws_security_group.instance_security_group.id]
 }
